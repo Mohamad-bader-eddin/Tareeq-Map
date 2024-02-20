e79320b76d4bd78e7d30b8eb5c899d36e4b2e505
Mohamad-bader-eddin <mohamad.bader.eddin.1@gmail.com>
2024-02-20 21:08:48 +0400

    fix router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { DarkModeProvider } from "./context/DarkMode.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <DarkModeProvider>
       <App />
     </DarkModeProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
