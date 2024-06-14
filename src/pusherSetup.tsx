55454411c609b78a3feafa8fa6f31c3b00cdc343
Mohamad-bader-eddin <mohamad.bader.eddin.1@gmail.com>
2024-06-14 18:12:05 +0300

    edit pusher setup

--- src/pusherSetup.tsx
+++ src/pusherSetup.tsx
@@ -1,7 +1,7 @@
 import Pusher from "pusher-js";
 
-const pusher = new Pusher("447631a51da812ef4f9d", {
-  cluster: "ap2",
+const pusher = new Pusher("621c2db7e47a1fde85df", {
+  cluster: "ap1",
 });
 
 export default pusher;
